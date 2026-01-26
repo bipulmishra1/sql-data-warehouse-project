513664355a005a77557796f9ebd1daba47a60cf7
bipul mishra <145924426+bipulmishra1@users.noreply.github.com>
2026-01-26 13:28:53 +0530

    Update ddl_bronze_creating_table.sql

--- scripts/bronze/ddl_bronze_creating_table.sql
+++ scripts/bronze/ddl_bronze_creating_table.sql
@@ -1,1 +1,110 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+*/
+-- Creating table in database: DataWarehouse
+
+-- Dropping table if already exists
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+-- Creating table of COUSTMER INFO
+
+Create table bronze.crm_cust_info
+(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(100),
+	cst_lastname NVARCHAR(100),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(10),
+	cst_create_date DATE
+);
+
+-- Dropping table if already exists
+
+if OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+
+-- Creating table of PRODUCT INFO
+
+Create table bronze.crm_prd_info
+(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(100),
+	prd_cost INT,
+	prd_line NVARCHAR(10),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+);
+
+-- Dropping table if already exists
+
+if OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+
+-- Creating table of SALES DETAILS
+
+Create table bronze.crm_sales_details
+(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+-- Dropping table if already exists
+
+if OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+
+-- Creating table of CUSTOMER 
+
+Create Table bronze.erp_cust_az12
+(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
+);
+
+-- Dropping table if already exists
+
+if OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+
+-- Creating table of location CUSTOMER
+
+Create Table bronze.erp_loc_a101
+(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
+
+-- Dropping table if already exists
+
+if OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+-- Creating table of PRODUCT CATEGORY
+
+Create Table bronze.erp_px_cat_g1v2
+(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+
+
+
+
+
 
